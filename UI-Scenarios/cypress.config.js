9a6e9f1989eca1fb0b1a12f18062216d2ac7c009
leticialsilva-exe <leticial.silva@outlook.com>
2026-03-20 21:55:13 -0300

    adjusting cypress UI tests

--- UI-Scenarios/cypress.config.js
+++ UI-Scenarios/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false, 
+    browser: 'electron',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
