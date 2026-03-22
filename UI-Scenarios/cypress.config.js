c290334f5bd1ecf4ede24680759e84db0ccc2090
leticialsilva-exe <leticial.silva@outlook.com>
2026-03-22 11:09:13 -0300

    runing mobile tests within sauceLabs

--- UI-Scenarios/cypress.config.js
+++ UI-Scenarios/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false, 
     browser: 'electron',
-    pageLoadTimeout: 120000,
+    pageLoadTimeout: 240000,
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
